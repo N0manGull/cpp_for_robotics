0196231eda84601985b3c9fbb11198d2e55c4f3d
N0manGull <nomi45407@gmail.com>
2026-02-25 19:24:25 +0500

    Refactor game features

--- 1_number_guessing_game.cpp
+++ 1_number_guessing_game.cpp
@@ -2,19 +2,33 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n = 0, m;
+    cout << "Welcome to Number Guessing Game." << endl; 
+    cout << "You have 3 chance to win. Good luck!" << endl;
+    bool guess = false;
+    while(n<3){
+    cout << "Attempts left: " << 3 - n << endl;
     cout << "Enter a guess number between 1 and 10: ";
-    cin >> n;
-    
-    if (n < 5){
+    cin >> m;
+        
+    if (m < 5){
         cout << "It's Too Low!" << endl;
     }
-    else if (n == 5){
+    else if (m == 5){
         cout << "Correct Guess" << endl;
+        guess = true;
+        break; 
     }
     else{
         cout << "It's Too High" << endl;
     }
-
+    n++;
+    }
+    if(guess == true){
+        cout << "Well Played." << endl;
+    }
+    else{
+        cout << "Game Over! Better luck next time." << endl;
+    } 
     return 0;
 }
